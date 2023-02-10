4ed6b63e9c2e43554c488ffb4a3c2c0701a0b00c
SMR <seyyedmortezarazavi76@gmail.com>
2023-02-10 14:39:26 +0330

    Add cog-outline icon.

--- launch.sh
+++ launch.sh
@@ -2,12 +2,12 @@
 # Open local server using default browser
 if [[ -n "$IS_WSL" || -n "$WSL_DISTRO_NAME" ]] # If wsl
 then
-	cmd.exe /c start http://127.0.0.1:800/icon-manager/index.php
+	cmd.exe /c start http://localhost:2345/icon-manager/index.php
 elif command -v xdg-open &> /dev/null # If linux
 then
-    xdg-open http://127.0.0.1:800/icon-manager/index.php
+    xdg-open http://localhost:2345/icon-manager/index.php
 else # If windows msys
-    start http://127.0.0.1:800/icon-manager/index.php
+    start http://localhost:2345/icon-manager/index.php
 fi
 cp -n sample.conf ../manager.conf
-php -S localhost:800 -t ../.
+php -S localhost:2345 -t ../.
